6215304677558cf0d6c67d72c1ab884ea600bbaa
Piyush Tyagi <tyagipiyush2233@gmail.com>
2022-10-11 11:20:04 +0530

    check-1

---
+++ index.js
@@ -0,0 +1,35 @@
+// var first = document.getElementsByTagName('Li')
+// var span = document.getElementsByTagName('span')
+
+// first.mouseover = function(){
+//     span.style.display  = 'block'
+// }
+// first.onmouseout = function(){
+//     span.style.display = 'none'
+// }
+
+// $(document).ready(function()
+// {
+
+// var pixelToMove = 50;
+
+// $("#background-image").mousemove(function(e)
+// {
+
+// var width = $(this).innerWidth();
+
+// var height = $(this).innerHeight();
+
+// var newValueX = (e.pageX / width) *
+// pixelToMove;
+
+// var newValueY = (e.pageY / height) *
+// pixelToMove;
+
+// $(this).css('background-position',
+// newValueX + '%' + ' ' + newValueY + '%');
+
+// });
+// const btn = document.getElementById('send
+
+ScrollReveal().version;
