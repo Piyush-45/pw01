015b1f9a367181e06940798b41b639d74d229b44
Piyush Tyagi <tyagipiyush2233@gmail.com>
2022-10-21 14:08:43 +0530

    image optimized

---
+++ ishu.py
@@ -0,0 +1,14 @@
+num1 = input("enter the num1: ")
+num2 = input("enter the num2: ")
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print(sum)
+
+#  product of two nos
+product = num1 * num2
+
+# printing values
+# print(product)
+ 
